e25b9f307f959e524a706fa961c429188342561f
Aleksandrs Jerasovs <aleksandrs.jerasovs@accenture.com>
2019-09-20 10:32:33 +0300

    SinglePage algorithm proper naming

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using GameOfLife;
 
 namespace GameOfLife
 {
@@ -6,37 +7,25 @@ namespace GameOfLife
     {
         public static void Main(string[] args)
         {
-            //Console.WriteLine("Please input field height: ");
-            //dishHeight = Convert.ToInt32(Console.ReadLine());
+            PopulateFieldRandomly();
 
-            //Console.WriteLine("Please input field width: ");
-            //dishWidth = Convert.ToInt32(Console.ReadLine());
-
-            PopulatePetriDishRandomly();
-
-            for (int i = 1; i < iterationsOfLife; i++)
+            for (int i = 1; i < iterationsOfGame; i++)
             {
-                printArray(petriDishInitial);
-                RepopulatePetriDish();
-                
+                printArray(fieldInitial);
+                RepopulateField();   
             }
+            Console.SetCursorPosition(0, fieldWidth);
+            Console.WriteLine("Game iterated {0} times", iterationsOfGame);
             Console.ReadLine();
         }
 
         static int threadDelay = 30;
-        static int iterationsOfLife = 300;
-        static int dishHeight = 28;
-        static int dishWidth = 28;
-        static bool[,] petriDishInitial = new bool[dishHeight, dishWidth];
-        //{ 
-        //    { false, false, false, false, false }, 
-        //    { false, false, false, false, false }, 
-        //    { false,  true,  true,  true, false }, 
-        //    { false, false, false, false, false },
-        //    { false, false, false, false, false }
-        //};
-        static bool[,] petriDishOutput = new bool[dishHeight, dishWidth];
-        static int populationIntensity = 3;
+        static int iterationsOfGame = 100;
+        static int fieldHeight = 28;
+        static int fieldWidth = 28;
+        static bool[,] fieldInitial = new bool[fieldHeight, fieldWidth];
+        static bool[,] fieldOutput  = new bool[fieldHeight, fieldWidth];
+        static int chanceToBeAlive = 2;
         const char livingCellSymbol = '*';
         const char deadCellSymbol = ' ';
         
@@ -45,8 +34,8 @@ namespace GameOfLife
         {
 
             int count = 0;
-            int rowLimit = dishHeight - 1;
-            int columnLimit = dishWidth - 1;
+            int rowLimit = fieldHeight - 1;
+            int columnLimit = fieldWidth - 1;
             for (int rowToCheck = row - 1; rowToCheck <= row + 1; rowToCheck++)
             {
                 for (int columnToCheck = column - 1; columnToCheck <= column + 1; columnToCheck++)
@@ -55,7 +44,7 @@ namespace GameOfLife
                           columnToCheck < 0 ||
                           rowToCheck > rowLimit ||
                           columnToCheck > columnLimit) && 
-                          petriDishInitial[rowToCheck, columnToCheck] == true)
+                          fieldInitial[rowToCheck, columnToCheck] == true)
                     {
                         count++;
                     }
@@ -78,58 +67,59 @@ namespace GameOfLife
             }
         }
 
-        public static void PopulatePetriDishRandomly()
+        public static void PopulateFieldRandomly()
         {
-            for (int i = 0; i < dishHeight; i++)
+            for (int i = 0; i < fieldHeight; i++)
             {
-                for (int j = 0; j < dishWidth; j++)
+                for (int j = 0; j < fieldWidth; j++)
                 {
-                    petriDishInitial[i, j] = PopulateCellRandomly(populationIntensity);
+                    fieldInitial[i, j] = PopulateCellRandomly(chanceToBeAlive);
                 }
             }
         }
 
-        public static void RepopulatePetriDish()
+        public static void RepopulateField()
         {
-            for (int i = 0; i < dishHeight; i++)
+            for (int i = 0; i < fieldHeight; i++)
             {
-                for (int j = 0; j < dishWidth; j++)
+                for (int j = 0; j < fieldWidth; j++)
                 {
                     int cellNeighbours = FindNeighbours(i, j);
-                    //int deadCellNeighbours = 9 - cellNeighbours;
-                    if ((cellNeighbours == 3 || cellNeighbours == 4) && petriDishInitial[i, j] == true)
-                    {
-                         
-                        petriDishOutput[i,j] = true;
+
+                    if ((cellNeighbours == 3 || 
+                         cellNeighbours == 4) && 
+                         fieldInitial[i, j] == true)
+                    {     
+                        fieldOutput[i,j] = true;
                     }
                     
-                    else if (cellNeighbours == 3 && petriDishInitial[i, j] == false)
+                    else if (cellNeighbours == 3 && 
+                             fieldInitial[i, j] == false)
                     {
-                        petriDishOutput[i, j] = true;
+                        fieldOutput[i, j] = true;
                     }
                     else
                     {
-                        petriDishOutput[i, j] = false;
+                        fieldOutput[i, j] = false;
                     }
                 }
             }
-            for (int i = 0; i < petriDishInitial.GetLength(0); i++)
+            for (int i = 0; i < fieldHeight; i++)
             {
-                for (int j = 0; j < petriDishInitial.GetLength(1); j++)
+                for (int j = 0; j < fieldWidth; j++)
                 {
-
-                    petriDishInitial[i, j] = petriDishOutput[i, j];
+                    fieldInitial[i, j] = fieldOutput[i, j];
                 }
             }                    
         }
 
-        public static void printArray( bool[,] pofig)
+        public static void printArray( bool[,] fieldToPrint)
         {
-            for(int i = 0; i < dishHeight; i++)
+            for(int i = 0; i < fieldHeight; i++)
             {
-                for ( int j = 0; j < dishWidth; j++)
+                for ( int j = 0; j < fieldWidth; j++)
                 {
-                    if (pofig[i, j] == true)
+                    if (fieldToPrint[i, j] == true)
                     {
                         Console.Write(livingCellSymbol + " ");
                     }
